82b61223c4efb1dcb7abff76b5956f0e7e1d7d01
muneeb042 <muneebmushtaq042@gmail.com>
2024-02-04 21:14:08 +0530

    complete day 1

---
+++ Day1/datatypes.js
@@ -0,0 +1,19 @@
+/*Primitive datatypes
+string , number , boolean, undefined, null and symbol. */
+
+// string --one or more characters within single, double and backticks
+console.log(typeof'muneeb');
+
+// numbers-- integers and float-point
+console.log(typeof 5);
+console.log(typeof -8.43);
+
+//boolean -- either true or false 
+console.log(typeof true);
+
+/* undefined --didn't assign a value to a variable , value is undefined
+function not returnong anything returns undefined */
+console.log(typeof undefined);
+
+//null in js means empty value
+console.log(typeof null);
---
+++ Day1/variables.js
@@ -0,0 +1,22 @@
+/*variables are named memory locations to store data.
+when variable is declared , memory locations are reserved. 
+when a variable is assigned to a data, memory space is filled with data. */
+
+// use var, let or const keywords
+
+/* follows camelCase , shouldn't begin with a number or a special char except dollar and underscore.*/
+
+// syntax : let nameOfVariable = value;
+let firstName = 'muneb';
+let lastName = 'ganie';
+let country = 'India';
+let city = 'Anantnag';
+let age = 24;
+let isMarried = false;
+
+console.log(firstName,lastName,country,city,age,isMarried);
+
+const gravity = 9.81;
+const pi = 3.14;
+
+console.log(gravity,pi);
