473f934e8348b7214351ff744edf0b31d7526212
muneeb042 <muneebmushtaq042@gmail.com>
2024-02-04 21:56:13 +0530

    primitive data types

---
+++ Day2/primitivedatatypes.js
@@ -0,0 +1,17 @@
+//primitive data types-- numbers, strings, boolean, null, undefined and symbol
+
+// primitive data types are immutabe
+let word = 'muneb';
+
+ word[0]='c' //gives error: strings are immutable
+
+//primitive data types are compared by their values
+
+let num1 = 3;
+let num2 = 5;
+console.log(num1==num2);
+
+let js= 'javascript';
+let py = 'python';
+
+console.log(js==py);
