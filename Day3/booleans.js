fee0d0c5ab86fd1503cf182a231cfe9bfc80d23e
muneeb042 <muneebmushtaq042@gmail.com>
2024-02-06 18:49:01 +0530

    day 3

---
+++ Day3/booleans.js
@@ -0,0 +1,32 @@
+//comparision returns boolean value
+let isHungry = false;
+let isMarried = true;
+let trueValue = 5>3; //true
+
+
+// truthy value
+/*      all numbers except 0 are truthy
+        all strings except an empty string '' are truthy 
+        the boolean true 
+ */
+
+
+ //falsy values
+ /*      0
+         null
+         undefined
+         NaN
+         the boolean false
+         empty string '', "", ``
+*/
+
+//undefined -- if we define a variable and if we don't assign any value, value will be undefined
+//if a functions is not returning any value, it will be undefined.
+let firstname;
+console.log(firstname); // undefined
+
+//Null
+let middlename = null;
+console.log(middlename); //null
+
+
---
+++ Day3/operators.js
@@ -0,0 +1,33 @@
+// assignment operators
+
+/*       operator
+
+         assignment   =
+         +=
+         -=
+         *=
+         /=
+         %=
+         **=
+
+*/
+
+
+//arithemetic operators
+
+/*      addition +
+        subtraction -
+        multiplication *
+        division /
+        modulus %
+        exponential **
+ */
+
+
+let initialVelocity = 10;
+let acceleration = 5;
+let time = 100;
+let finalVelocity = initialVelocity + acceleration*time;
+console.log(finalVelocity);
+console.log(`The final velocity of a body which has initial velocity ${initialVelocity}m/s \n \
+moving with an acceleration of ${acceleration}m/s2 after time ${time}s will be ${finalVelocity}m/s`);
