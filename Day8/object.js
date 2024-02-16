e6958064ddf7cae20ea93b29b1659ddfb6fa07b7
muneeb042 <muneebmushtaq042@gmail.com>
2024-02-16 12:42:41 +0530

    object

---
+++ Day8/object.js
@@ -0,0 +1,24 @@
+//objects have properties and values i,e. key value pair
+//order of key is not reserved
+//to create an object literal use curly brackets
+
+const person = {}//empty object
+
+//creating an object with values
+const rectangle ={
+    length:30,
+    breadth :80
+};
+console.log(rectangle);
+
+
+const myself = {
+    firstName:'Muneeb',
+    secondName:'Ahmad',
+    lastName: 'Ganie',
+    age: '24',
+    country: 'India',
+    skils : ['Html','Css','javascript','C','C++','Python'],
+    isMarried : false
+};
+console.log(person);
